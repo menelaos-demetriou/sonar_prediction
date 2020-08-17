618a23aefc2a82023fe248931dab05a7f7f25943
Menelaos Dimitriou <menelaos.dimitrio@gmail.com>
2020-08-17 20:00:45 +0100

    Finished training

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Sonar Prediction
 The task is to train a predictor to discriminate between sonar signals bounced off a metal cylinder and those bounced off a roughly cylindrical rock.
 More information can be found in https://archive.ics.uci.edu/ml/datasets/Connectionist+Bench+(Sonar,+Mines+vs.+Rocks)
+Accuracy on the test set  0.6226415094339622  
 
 # Further improvements:
 Add more models for grid search.  
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import pickle
 
 from sklearn import metrics
+import keras
 from keras.layers import Dense
 from keras import backend as K
 from keras.optimizers import SGD
@@ -8,6 +10,8 @@ from keras.layers import Dropout
 from keras.models import Sequential
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import RFECV
+from ann_visualizer.visualize import ann_viz
+from keras.utils.vis_utils import plot_model
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
@@ -20,6 +24,7 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 GRID = True
+
 
 def feature_selection(data, type):
     y = data[60].copy()
@@ -47,11 +52,11 @@ def feature_selection(data, type):
 
         X = X.loc[:, list(rfecv.support_)]
         # Return dataframe with best features and target
-        return pd.concat([X, pd.Series(y, name="target")], axis = 1)
+        return pd.concat([X, pd.Series(y, name="target")], axis=1)
     elif type == "pca":
         pca = PCA(n_components=0.95)
         x_reduced = pca.fit_transform(X)
-        return pd.concat([pd.DataFrame(x_reduced), pd.Series(y, name="target")], axis = 1)
+        return pd.concat([pd.DataFrame(x_reduced), pd.Series(y, name="target")], axis=1)
 
 
 def create_model(num, dropout_rate=0.1, neurons1=10, neurons2=5, init_mode='uniform', learn_rate=0.01, momentum=0,
@@ -62,11 +67,11 @@ def create_model(num, dropout_rate=0.1, neurons1=10, neurons2=5, init_mode='unif
     model.add(Dense(neurons1, input_dim=num, kernel_initializer=init_mode, activation='relu'))
     model.add(Dropout(dropout_rate))
     model.add(Dense(neurons2, kernel_initializer=init_mode, activation='relu'))
-    model.add(Dropout(dropout_rate))
     model.add(Dense(1, kernel_initializer=init_mode, activation='sigmoid'))
     optimizer = SGD(lr=learn_rate, momentum=momentum)
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     return model
+
 
 def main():
     # Read file with dataset
@@ -78,7 +83,18 @@ def main():
     final_data = feature_selection(data, "pca")
 
     # Create model
+    ann_model = create_model(num=len(final_data.columns)-1)
+    early_stopping = keras.callbacks.EarlyStopping(
+        verbose=1,
+        patience=10,
+        mode='max',
+        restore_best_weights=True)
+
     model = KerasClassifier(build_fn=create_model, num=len(final_data.columns)-1, verbose=1)
+
+    ann_model.summary()
+    plot_model(ann_model, to_file='plots/model_plot.png', show_shapes=True, show_layer_names=True)
+    ann_viz(ann_model, filename="plots/neural_network.png", title="NN Architecture")
 
     # Split train and test set
     X = final_data.loc[:, ~final_data.columns.isin(["target"])]
@@ -86,31 +102,33 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     # Setting a dictionary with hyperparameters
-    epochs = [10, 50, 100, 150, 200]
-    optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
-    learn_rate = [0.001, 0.01, 0.1, 0.2, 0.3]
-    momentum = [0.0, 0.2, 0.4, 0.6, 0.8, 0.9]
-    init_mode = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal',
-                 'he_uniform']
-    dropout_rate = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    neurons1 = [1, 5, 10, 15, 20]
-    neurons2 = [1, 5, 10, 15, 20]
+    epochs = [200]
+    optimizer = ['RMSprop', 'Adam', 'Nadam']
+    learn_rate = [0.0001, 0.001]
+    momentum = [0.2, 0.4, 0.6, 0.8, 0.9]
+    init_mode = ['lecun_uniform', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
+    dropout_rate = [0.3, 0.4, 0.5]
+    neurons1 = [15, 20]
+    neurons2 = [15, 20]
 
     param_grid = dict(epochs=epochs, optimizer=optimizer, learn_rate=learn_rate, momentum=momentum,
                       init_mode=init_mode, dropout_rate=dropout_rate, neurons1=neurons1, neurons2=neurons2)
 
-
     if GRID:
         grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=5)
-        grid_result = grid.fit(X_train, y_train)
+        grid_result = grid.fit(X_train, y_train, **{'callbacks': [early_stopping]})
 
         # summarize results
-        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+        print("Best score: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
         means = grid_result.cv_results_['mean_test_score']
         stds = grid_result.cv_results_['std_test_score']
         params = grid_result.cv_results_['params']
         for mean, stdev, param in zip(means, stds, params):
             print("%f (%f) with: %r" % (mean, stdev, param))
+
+        print("Saving best estimator for evaluations on test dataset")
+        filename = "model/best_estimator.sav"
+        pickle.dump(grid_result.best_estimator_, open(filename, 'wb'))
 
         y_pred_class = grid_result.best_estimator_.predict(X_test)
         print("Accuracy on the test set ", metrics.accuracy_score(y_test, y_pred_class))
